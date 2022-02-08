1b687e824c977d42f06c52dfa155a28fe250f9aa
vitor <tutornews2@gmail.com>
2022-02-08 14:05:54 -0300

    deeploy

---
+++ build/precache-manifest.af64f6c4a24c5375e8d0520fd1ca23f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b98fbd56796ba6d136084845506c32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fed913400379d92ca271",
+    "url": "/static/css/main.56328471.chunk.css"
+  },
+  {
+    "revision": "9e5777e10d2eac6d395f",
+    "url": "/static/js/2.bf328627.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bf328627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed913400379d92ca271",
+    "url": "/static/js/main.67b2ddd5.chunk.js"
+  },
+  {
+    "revision": "3d22c4ff75fc8bca25f7",
+    "url": "/static/js/runtime-main.68c0a999.js"
+  }
+]);
